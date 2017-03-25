7f35d2d6f9dea43f4a6167eccfe670ecc0a6f7f5
= <you@example.com>
2017-03-25 23:26:40 +0530

    polls added on admin

---
+++ myapp/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+   question_text=models.TextField(max_length=300)
+   pub_date=models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+   question=models.ForeignKey(Question)
+   choice_text=models.CharField(max_length=200)
+   votes=models.IntegerField(default=0)
+
+#def __str__(self):
+ #  return self.question_text
+#def__str__(self):
+
